c9602f37cd2f555468d748933b147417c21ea0dd
pixelgd-games <pixelgd.games@gmail.com>
2026-09-25 18:30:13 +0800

    Raise Monster Lab round payout cap to 100M POINT

---
+++ supabase/migrations/20260925120000_monster_lab_round_payout.sql
@@ -0,0 +1,17 @@
+begin;
+
+do $$
+declare v_count integer;
+begin
+  update public.joy8_game_policies p set max_payout_amount=100000000
+  from public.games g
+  where g.id=p.game_id and g.slug='monster-lab'
+    and p.funding_mode='platform' and p.max_bet_amount=10000 and p.max_payout_amount=5000000;
+  get diagnostics v_count=row_count;
+  if v_count<>1 then
+    raise exception 'JOY8_MONSTER_LAB_POLICY_NOT_READY' using errcode='55000';
+  end if;
+end;
+$$;
+
+commit;
